6533f4d597daf107d1142c6084ed230c331724b4
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-11-18 17:12:17 +0530

    Update ColorCoding.c

--- ColorCoding.c
+++ ColorCoding.c
@@ -12,6 +12,10 @@ const char* MinorColorNames[] = {
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
+static void ColorPairToString(const ColorPair* colorPair, char* buffer);
+static int GetPairNumberFromColor(const ColorPair* colorPair);
+static void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+
 
 
 
