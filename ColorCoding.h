818ee15e9c8ecae6b661fd06d6b07077e5a51c22
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-11-18 19:06:31 +0530

    Update ColorCoding.h

--- ColorCoding.h
+++ ColorCoding.h
@@ -8,5 +8,5 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
-void testPairToNumber(enum MajorColor major,enum MinorColor minor, int expectedPairNumber);
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+//void testPairToNumber(enum MajorColor major,enum MinorColor minor, int expectedPairNumber);
+//void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
