3b3a7b3213b5310f29941aa892eb2dbeca24d97e
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-11-18 18:55:53 +0530

    Update ColorCoding.c

--- ColorCoding.c
+++ ColorCoding.c
@@ -15,6 +15,27 @@ int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 static void ColorPairToString(const ColorPair* colorPair, char* buffer);
 static int GetPairNumberFromColor(const ColorPair* colorPair);
 
+static int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
+
+static void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
+
+static ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
 
 
 
