73cb94547e75a58f71bd5258c0e679745c7452da
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-11-18 17:00:08 +0530

    Update ColorCoding.h

--- ColorCoding.h
+++ ColorCoding.h
@@ -1,1 +1,9 @@
 
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
